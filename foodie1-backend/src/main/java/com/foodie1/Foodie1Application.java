69f43865349e8f72915fe384182cda94894909a5
HungDao03 <daohung151003@gmail.com>
2025-08-13 15:45:07 +0700

    22222

--- foodie1-backend/src/main/java/com/foodie1/Foodie1Application.java
+++ foodie1-backend/src/main/java/com/foodie1/Foodie1Application.java
@@ -10,6 +10,9 @@ public class Foodie1Application {
 
     public static void main(String[] args) {
         System.out.println("Base URL: " + System.getenv("baseUrl")); // Log ra biến môi trường
+        System.out.println("GOOGLE_CLIENT_ID: " + System.getenv("GOOGLE_CLIENT_ID"));
+        System.out.println("GOOGLE_CLIENT_SECRET: " + System.getenv("GOOGLE_CLIENT_SECRET"));
+        System.out.println("Frontend URL from env: " + System.getenv("FRONTEND_URL"));
         SpringApplication.run(Foodie1Application.class, args);
     }
     
